92a74867d2c9f0ccba1ffb415e2462449a37f621
Vitor Bortolotti Ribeiro <vitor.ribeiro@ifes.edu.br>
2019-03-12 17:26:04 -0300

    Added Vue to Webpacker and created the index page to serve the app as a SPA

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    root 'pages#index'
+    get '404', to: 'pages#index'
+
+    namespace :api do
+
+    end
 end
