c9c84df6b146e8df3126644e58a2d460be53c0a2
vitor.ribeiro <bortolotti.vitor@gmail.com>
2019-03-21 17:19:37 -0300

    Adding ID field to ProductType

--- app/graphql/types/product_type.rb
+++ app/graphql/types/product_type.rb
@@ -2,6 +2,7 @@ module Types
     class ProductType < Types::BaseObject
         description "A product from the store"
 
+        field :id, ID, null: false
         field :name, String, null: false
         field :price, Float, null: false
         field :brand, BrandType, null: false
