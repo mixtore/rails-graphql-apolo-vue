3ca55ca20feac10d249841b992295e4b95866d80
vitor <bortolotti.vitor@gmail.com>
2019-03-21 07:47:34 -0300

    Removing api namespace

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,12 @@ Rails.application.routes.draw do
   root 'pages#index'
   get '404', to: 'pages#index'
 
-  namespace :api do
-
-  end
-
   if Rails.env.development?
     mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
   end
+
   post "/graphql", to: "graphql#execute"
+
+  get '/*path', to: 'pages#index', format: false
+
 end
